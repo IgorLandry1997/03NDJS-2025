5eafc1d0d890d491dc92e6a5fc3d9ef2fc07d4db
IgorLandry1997 <njoungangigor@gmail.com>
2025-04-17 00:09:45 +0100

    MVC Tutorial

---
+++ Sandbox/first_step/main.js
@@ -0,0 +1,6 @@
+const { sum, diff, prod, quot } = require('./math.js');
+
+console.log("Sum (5+3):", sum(5, 3));
+console.log("Difference (5-3):", diff(5, 3));
+console.log("Product (5*3):", prod(5, 3));
+console.log("Quotient (5/3):", quot(5, 3));
